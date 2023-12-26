591dd7f80010037bea997a75e796047891923ba6
Jakub Kanna <hello.jakubkanna@gmail.com>
2023-12-26 17:23:51 +0100

    generates possiblemoves

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-class Vertex {
-  constructor(data) {
-    this.data = data;
-    this.adjacents = this.possibleMoves();
-  }
+function generatePossibleMoves(vertex) {
+  const [x, y] = vertex;
 
-  static knightOffsets = [
+  //possible knight moves offsets
+  const offsets = [
     [1, 2],
     [1, -2],
     [2, 1],
@@ -15,16 +13,45 @@ class Vertex {
     [-2, -1],
   ];
 
-  possibleMoves() {
-    //return list of offseted Vertexes
-    //each Vertex should have it's own list of offseted Vertexes
-    //recursively update the list, until x or y is bigger than 8 (new position is not within the chessboard)
+  //list of moves for the vertex
+  const moves = [];
+
+  for (const offset of offsets) {
+    const newX = offset[0] + x;
+    const newY = offset[1] + y;
+    if (onBoard(newX, newY)) moves.push([newX, newY]); //check if is on the board
+  }
+
+  return moves;
+}
+
+const onBoard = (x, y) => x >= 0 && x < 8 && y >= 0 && y < 8;
+
+function knightTravails(start, end) {
+  if (!onBoard(start) && onBoard(end))
+    return "Start or end vertex is not on the board.";
+
+  const queue = [start];
+  const visited = new Set(); //remove duplicates
+
+  while (queue.length > 0) {
+    // dequeue
+    const current = queue.shift();
+
+    if (current[0] === end[0] && current[1] === end[1]) {
+      return `You made it in ... moves!  Here's your path: ...`;
+    }
+
+    visited.add(current);
+
+    // enqueue
+    const possibleMoves = generatePossibleMoves(current);
+    for (const move of possibleMoves) {
+      if (!visited.has(move)) {
+        queue.push(move);
+      }
+    }
   }
 }
 
-function knightMoves(start, finish) {
-  const vertex = new Vertex(start);
-  console.log(vertex);
-}
-
-knightMoves([3, 3], [0, 0]);
+console.log(knightTravails([0, 0], [3, 3]));
