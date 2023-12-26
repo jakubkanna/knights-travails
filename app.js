819f158a144803d7af85408851b42c0490e0da72
Jakub Kanna <hello.jakubkanna@gmail.com>
2023-12-26 17:47:12 +0100

    solved with Breadth-First Search
    cleanup
    
    cleanup

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+// Function to generate possible moves for a given vertex [x, y]
 function generatePossibleMoves(vertex) {
   const [x, y] = vertex;
 
-  //possible knight moves offsets
+  // Possible knight moves offsets
   const offsets = [
     [1, 2],
     [1, -2],
@@ -13,45 +14,59 @@ function generatePossibleMoves(vertex) {
     [-2, -1],
   ];
 
-  //list of moves for the vertex
+  // List of moves
   const moves = [];
 
+  // Process for the vertex x, y
   for (const offset of offsets) {
     const newX = offset[0] + x;
     const newY = offset[1] + y;
-    if (onBoard(newX, newY)) moves.push([newX, newY]); //check if is on the board
+    if (onBoard(newX, newY)) moves.push([newX, newY]); // Check if it is on the board
   }
 
   return moves;
 }
 
+// Check if x, y is inside an 8x8 board
 const onBoard = (x, y) => x >= 0 && x < 8 && y >= 0 && y < 8;
 
+// Breadth-First Search
 function knightTravails(start, end) {
-  if (!onBoard(start) && onBoard(end))
-    return "Start or end vertex is not on the board.";
+  // Check if start and end are on the board
+  if (!onBoard(start) && onBoard(end)) return "No valid path found.";
 
-  const queue = [start];
-  const visited = new Set(); //remove duplicates
+  const queue = [[start]];
+  const visited = new Set(); // Use Set to remove duplicates
 
   while (queue.length > 0) {
-    // dequeue
-    const current = queue.shift();
+    // Dequeue
+    const currentPath = queue.shift(); // Remove and save the first element of the queue
+    const current = currentPath[currentPath.length - 1]; // The last element of currentPath
 
+    // If the end has been found
     if (current[0] === end[0] && current[1] === end[1]) {
-      return `You made it in ... moves!  Here's your path: ...`;
+      // Render positions [x, y] and join them with ->
+      return `You made it in ${
+        currentPath.length - 1
+      } moves! Here's your path: ${currentPath
+        .map((vertex) => `[${vertex.join(", ")}]`)
+        .join(" -> ")}`;
     }
 
     visited.add(current);
 
-    // enqueue
+    // Enqueue
     const possibleMoves = generatePossibleMoves(current);
+    // For each possibleMove of the current vertex, create a new path
     for (const move of possibleMoves) {
       if (!visited.has(move)) {
-        queue.push(move);
+        const newPath = [...currentPath, move]; //for every move add it's path
+        queue.push(newPath);
       }
     }
   }
+
+  return "No valid path found.";
 }
 
 console.log(knightTravails([0, 0], [3, 3]));
